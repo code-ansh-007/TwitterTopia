cccf8d9b75be840ef15fd520eb8e7f07b739347e
Ansh Pradhan <anshpradhancareer03@gmail.com>
2024-06-09 15:33:49 +0530

    check 2

--- client/src/components/TweetCard.tsx
+++ client/src/components/TweetCard.tsx
@@ -13,8 +13,15 @@ const TweetCard = ({ tweet }: TweetCardProps) => {
           alt="user pic"
           className="w-8 h-8 rounded-full"
         />
-        <span></span>
+        <span>Hello</span>
       </div>
+      <span className="text-sm">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+        ducimus fugit minus, a quaerat ut enim officiis deserunt autem
+        cupiditate labore dolores. Perspiciatis dolor possimus explicabo
+        delectus tempora amet modi ipsa, minus nisi quidem rem asperiores aut in
+        itaque doloribus earum, non vel laudantium ab.
+      </span>
     </main>
   );
 };
